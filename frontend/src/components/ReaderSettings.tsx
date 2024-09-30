a1fef9218732ffefaaa45886eba93e51b1c9c643
KJdotIO <98490529+KJdotIO@users.noreply.github.com>
2024-09-30 01:39:15 +0100

    updated some image issues

---
+++ frontend/src/components/ReaderSettings.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReaderSettings() {
+  return (
+    <div>
+        <button className='p-2 border-1 border-black'>Settings</button>
+    </div>
+  )
+}
+
+export default ReaderSettings
